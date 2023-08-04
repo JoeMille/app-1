d0ccb7fe64d35e9bf7f58d25eb8e9e096ccae646
JoeMille <chefjoemiller1992@gmail.com>
2023-08-04 14:41:07 +0000

    Changed timer function to display messages every 20 seconds during the meditation process and changed the HTML structure for the container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,80 @@
 // script.js
 
-//RELEVANT CODE FOR TIMER ON MEDITATION PAGE
-
-let timer;
-let timerDuration;
-
-function startTimer(minutes) {
+const messages = [
+    "Take a deep breath and relax.",
+    "Focus on the sensation of your breath.",
+    "Allow any thoughts to come and go without judgment.",
+    "Embrace this moment of stillness and peace.",
+    "You are in control of your thoughts and emotions.",
+    "Continue to breathe deeply and calmly.",
+    "When you're ready, gently open your eyes and return to the present moment."
+  ];
+  
+  let timer;
+  let timerDuration;
+  let messageIndex = 0;
+  let messageTimeout;
+  
+  function startTimer(minutes) {
     if (timer) {
-        clearInterval(timer);
+      clearInterval(timer);
     }
-
+  
     timerDuration = minutes * 60;
     updateTimerDisplay();
-
-    timer = setInterval(updateTimer, 1000);
-}
-
-function updateTimer() {
-    if (timerDuration <= 0) {
+    showMessage(messages[messageIndex]);
+  
+    timer = setInterval(() => {
+      timerDuration--;
+      updateTimerDisplay();
+  
+      if (timerDuration === 0) {
         clearInterval(timer);
-        alert("Meditation timer is complete!");
-        return;
-    }
-
-    timerDuration--;
-    updateTimerDisplay();
-}
-
-function updateTimerDisplay() {
+        showMessage("Meditation timer is complete!");
+      } else if (timerDuration % 20 === 0) {
+        // Show a new message every 20 seconds
+        messageIndex++;
+        showMessage(messages[messageIndex]);
+      }
+    }, 1000);
+  }
+  
+  function updateTimerDisplay() {
     const minutesDisplay = document.getElementById("minutes");
     const secondsDisplay = document.getElementById("seconds");
-
+  
     const minutes = Math.floor(timerDuration / 60).toString().padStart(2, "0");
     const seconds = (timerDuration % 60).toString().padStart(2, "0");
-
+  
     minutesDisplay.textContent = minutes;
     secondsDisplay.textContent = seconds;
-}
-
-function stopTimer() {
+  }
+  
+  function showMessage(message) {
+    const messagesContainer = document.querySelector(".messages-container");
+  
+    // Clear previous message timeout if it exists
+    clearTimeout(messageTimeout);
+  
+    // Clear previous messages if any
+    while (messagesContainer.firstChild) {
+      messagesContainer.removeChild(messagesContainer.firstChild);
+    }
+  
+    const messageContainer = document.createElement("div");
+    messageContainer.textContent = message;
+    messageContainer.className = "message";
+  
+    messagesContainer.appendChild(messageContainer);
+  
+    // Hide the message after 10 seconds
+    messageTimeout = setTimeout(() => {
+      messageContainer.style.display = "none";
+    }, 10000);
+  }
+  
+  function stopTimer() {
     clearInterval(timer);
     updateTimerDisplay();
-}
-//-----------------------------------------------------------
+  }
+  
