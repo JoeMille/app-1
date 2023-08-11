337a86c17569d902aaaf1e7caa9326597164b2f1
Joe Miller <98966875+JoeMille@users.noreply.github.com>
2023-08-11 12:26:13 +0100

    removed the JS code forcing meditation messages to disappear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,78 @@
 // timer function for meditation page
 
 const messages = [
-    "Take three deep breaths....",
-    "Focus on your breathing....",
-    "Begin to remove any intruding thoughs, good or bad...",
-    "Embrace this tranquility....",
-    "Continue to breathe deeply....",
-    "Focus on the quiet....",
-    "Keep your shoulders relaxed and your head high....",
-    "Continue to breathe deeply, clearing any further thoughts from your mind....",
-    "Bring your hands together and stretch to the sky....",
-    "Lean your head back and hold your breath for five seconds....",
-    "Close your eyes and imagine immersing into calm water....",
-    
-  ];
-  
-  let timer;
-  let timerDuration;
-  let messageIndex = 0;
-  let messageTimeout;
-  
-  function startTimer(minutes) {
-    if (timer) {
-      clearInterval(timer);
-    }
-  
-    timerDuration = minutes * 60;
-    updateTimerDisplay();
-    showMessage(messages[messageIndex]);
-  
-    timer = setInterval(() => {
-      timerDuration--;
-      updateTimerDisplay();
-  
-      if (timerDuration === 0) {
-        clearInterval(timer);
-        showMessage("Meditation timer is complete!");
-      } else if (timerDuration % 20 === 0) {
-        // Show a new message every 20 seconds
-        messageIndex++;
-        showMessage(messages[messageIndex]);
-      }
-    }, 1000);
-  }
-  
-  function updateTimerDisplay() {
-    const minutesDisplay = document.getElementById("minutes");
-    const secondsDisplay = document.getElementById("seconds");
-  
-    const minutes = Math.floor(timerDuration / 60).toString().padStart(2, "0");
-    const seconds = (timerDuration % 60).toString().padStart(2, "0");
-  
-    minutesDisplay.textContent = minutes;
-    secondsDisplay.textContent = seconds;
-  }
-  
-  function showMessage(message) {
-    const messagesContainer = document.querySelector(".messages-container");
-  
-    // Clear previous message timeout if it exists
-    clearTimeout(messageTimeout);
-  
-    // Clear previous messages 
-    while (messagesContainer.firstChild) {
-      messagesContainer.removeChild(messagesContainer.firstChild);
-    }
-  
-    const messageContainer = document.createElement("div");
-    messageContainer.textContent = message;
-    messageContainer.className = "message";
-  
-    messagesContainer.appendChild(messageContainer);
-  
-    // Hide the message after 10 seconds
-    messageTimeout = setTimeout(() => {
-      messageContainer.style.display = "none";
-    }, 10000);
-  }
-  
-  function stopTimer() {
+  "Take three deep breaths....",
+  "Focus on your breathing....",
+  "Begin to remove any intruding thoughs, good or bad...",
+  "Embrace this tranquility....",
+  "Continue to breathe deeply....",
+  "Focus on the quiet....",
+  "Keep your shoulders relaxed and your head high....",
+  "Continue to breathe deeply, clearing any further thoughts from your mind....",
+  "Bring your hands together and stretch to the sky....",
+  "Lean your head back and hold your breath for five seconds....",
+  "Close your eyes and imagine immersing into calm water....",
+];
+
+let timer;
+let timerDuration;
+let messageIndex = 0;
+
+function startTimer(minutes) {
+  if (timer) {
     clearInterval(timer);
-    updateTimerDisplay();
   }
-  
+
+  timerDuration = minutes * 60;
+  updateTimerDisplay();
+  showMessage(messages[messageIndex]);
+
+  timer = setInterval(() => {
+    timerDuration--;
+    updateTimerDisplay();
+
+    if (timerDuration === 0) {
+      clearInterval(timer);
+      showMessage("Meditation timer is complete!");
+    } else if (timerDuration % 20 === 0) {
+      // Show a new message every 20 seconds
+      messageIndex++;
+      showMessage(messages[messageIndex]);
+    }
+  }, 1000);
+}
+
+function updateTimerDisplay() {
+  const minutesDisplay = document.getElementById("minutes");
+  const secondsDisplay = document.getElementById("seconds");
+
+  const minutes = Math.floor(timerDuration / 60).toString().padStart(2, "0");
+  const seconds = (timerDuration % 60).toString().padStart(2, "0");
+
+  minutesDisplay.textContent = minutes;
+  secondsDisplay.textContent = seconds;
+}
+
+function showMessage(message) {
+  const messagesContainer = document.querySelector(".messages-container");
+
+  // Clear previous messages
+  while (messagesContainer.firstChild) {
+    messagesContainer.removeChild(messagesContainer.firstChild);
+  }
+
+  const messageContainer = document.createElement("div");
+  messageContainer.textContent = message;
+  messageContainer.className = "message";
+
+  messagesContainer.appendChild(messageContainer);
+}
+
+function stopTimer() {
+  clearInterval(timer);
+  updateTimerDisplay();
+}
+
   //--------------------------GOOGLE MAPS ON FIND US PAGE
 
   function initMap() {
@@ -93,10 +83,9 @@ const messages = [
     
     var googlemap = new google.maps.Map(document.getElementById("map"), mapOptions);
   
-    // Add a marker to the map
     var marker = new google.maps.Marker({
       position: { lat: 50.51151297284177, lng: -3.6323162471725485 },
-      map: googlemap, // Associate the marker with the map
+      map: googlemap, 
       title: "Head Set Zone", 
     });
   }
